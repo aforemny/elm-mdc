ef7d2a99166d82b789708e1155df86016b814c7e
Håkon Rossebø <hakonrossebo@hotmail.com>
2016-03-19 18:23:47 +0100

    feat(badge):initial implementation

--- examples/Demo/Badges.elm
+++ examples/Demo/Badges.elm
@@ -1,11 +1,18 @@
 module Demo.Badges where
 
--- import Material.Badge exposing (..)
+import Material.Icon as Icon
+import Material.Badge as Badge
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 view : List Html
 view =
   [ 
-    h1 [][text "Badges"]
+    h1 [][text "Badges"],
+    Icon.viewWithOptions "add" Icon.S18 [] { badgeInfo = Just (Badge.viewDefault "16")},
+    Icon.viewWithOptions "add" Icon.S18 [] { badgeInfo = Just (Badge.view   { overlap = False, noBackground = False} "99")},
+    Icon.viewWithOptions "add" Icon.S18 [] { badgeInfo = Just (Badge.view   { overlap = False, noBackground = True} "4")},
+    Icon.view "add" Icon.S18 [],
+    Icon.i "add"
   ]
---
+++ src/Material/Badge.elm
@@ -0,0 +1,87 @@
+module Material.Badge
+  ( Options
+  , view
+  , viewDefault
+  , BadgeInfo
+  ) where
+
+{-| From the [Material Design Lite documentation](http://www.getmdl.io/components/#badges-section):
+
+The Material Design Lite (MDL) badge component is an onscreen notification element. A badge consists of a small circle, typically containing a number or other characters, that appears in proximity to another object. A badge can be both a notifier that there are additional items associated with an object and an indicator of how many items there are.
+
+You can use a badge to unobtrusively draw the user's attention to items they might not otherwise notice, or to emphasize that items may need their attention. For example:
+
+A "New messages" notification might be followed by a badge containing the number of unread messages.
+A "You have unpurchased items in your shopping cart" reminder might include a badge showing the number of items in the cart.
+A "Join the discussion!" button might have an accompanying badge indicating the number of users currently participating in the discussion.
+A badge is almost always positioned near a link so that the user has a convenient way to access the additional information indicated by the badge. However, depending on the intent, the badge itself may or may not be part of the link.
+
+Badges are a new feature in user interfaces, and provide users with a visual clue to help them discover additional relevant content. Their design and use is therefore an important factor in the overall user experience.
+
+@docs viewDefault, view, BadgeInfo, Options
+-}
+
+
+import Html exposing (Attribute)
+import Html.Attributes exposing (attribute)
+
+
+{-| Internal Type used by other elements. Not sure if this should be documented.
+-}
+type alias BadgeInfo =
+  { classes : String
+  , dataBadge : Attribute
+  }
+
+{-| Options for Badge
+  ovelap: Bool to set css class - mdl-badge--overlap.	Make the badge overlap with its container
+  noBackground: Bool to set css class - mdl-badge--no-background.	Applies open-circle effect to badge
+-}
+type alias Options =
+  { overlap : Bool
+  , noBackground : Bool
+  }
+
+defaultOptions : Options
+defaultOptions =
+  { 
+    overlap = True
+  , noBackground = False
+  }
+
+{-| View function for Badge
+    
+    First parameter will set Badge options. See Options for more info.
+    Last parameter will set a value to data-badge="value".	Assigns string value to badge
+
+    import Material.Badge as Badge
+
+    badgeInfo : Badge.BadgeInfo
+    badgeInfo = Badge.view { overlap = False, noBackground = False} "3"
+-}
+view : Options -> String -> BadgeInfo
+view options databadge = 
+  let classes =
+    [
+      {css = " mdl-badge", show = True} 
+    , {css = " mdl-badge--no-background", show = options.noBackground}
+    , {css = " mdl-badge--overlap", show = options.overlap}
+    ]
+      |> List.filter .show
+      |> List.map .css
+      |> List.foldl (++) ""
+  in
+    { classes = " mdl-badge" ++ classes, dataBadge = attribute "data-badge" databadge}  
+
+
+{-| Create BadgeInfo with default values
+    
+    Parameter will set a value to data-badge="value".	Assigns string value to badge
+
+    import Material.Badge as Badge
+
+    badgeInfo : Badge.BadgeInfo
+    badgeInfo = Badge.viewDefault "3"
+-}
+viewDefault : String -> BadgeInfo
+viewDefault databadge = view defaultOptions databadge 
