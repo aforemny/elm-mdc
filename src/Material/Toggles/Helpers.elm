36900789cfe7b1f00717eb3cbf252f21489628e9
Søren Debois <debois@itu.dk>
2016-07-15 12:50:26 +0200

    Ripple animations now display in firefox. Closes #78.

--- src/Material/Toggles/Helpers.elm
+++ src/Material/Toggles/Helpers.elm
@@ -7,7 +7,7 @@ import Json.Decode as Decode
 
 import Material.Helpers exposing (blurOn)
 import Material.Ripple as Ripple
-import Material.Style exposing (styled, cs, cs', attribute, multiple)
+import Material.Style exposing (styled, cs, attribute, multiple)
 
 
 top : String -> Address Msg -> Model -> List Style -> List Html -> Html
@@ -15,10 +15,10 @@ top name addr model styles elems =
   styled label 
     [ cs ("mdl-" ++ name) 
     , cs ("mdl-js-" ++ name)
-    , cs' "mdl-js-ripple-effect" model.ripple
-    , cs' "mdl-js-ripple-effect--ignore-events" model.ripple
+    , cs "mdl-js-ripple-effect" `when` model.ripple
+    , cs "mdl-js-ripple-effect--ignore-events" `when` model.ripple
     , cs "is-upgraded"
-    , cs' "is-checked" model.value
+    , cs "is-checked" `when` model.value
     , attribute <| on "change" Decode.value (always (message addr Change))
     , attribute <| blurOn "mouseup"
     , attribute <| onFocus addr (SetFocus True)
