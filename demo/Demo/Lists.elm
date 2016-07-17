a38f4f17cb388d29fc6533da046bc696ac144a58
Håkon Rossebø <hakonrossebo@hotmail.com>
2016-07-17 20:53:23 +0200

    Lists implementation with demo working

--- demo/Demo/Lists.elm
+++ demo/Demo/Lists.elm
@@ -5,13 +5,14 @@ import Html exposing (..)
 
 import Material.Lists as Lists
 import Material 
+import Material.Options as Options 
+import Material.Icon as Icon
+import Material.Toggles as Toggles
 
 import Demo.Page as Page
-
+import Demo.Code as Code
 
 -- MODEL
-
-
 type alias Mdl = 
   Material.Model 
 
@@ -28,8 +29,6 @@ model =
 
 
 -- ACTION, UPDATE
-
-
 type Msg 
   = ListsMsg 
   | Mdl Material.Msg 
@@ -46,13 +45,264 @@ update action model =
 
 
 -- VIEW
+simpleList : Model -> Html Msg
+simpleList model  =
+    Lists.ul [Options.css "width" "300px"] 
+      [   
+        Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Bryan Cranston"]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Aaron Paul"]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Bob Odenkirk"]
+          ]
+      ]
+
+
+icons : Model -> Html Msg
+icons model  =
+    Lists.ul [Options.css "width" "300px" ] 
+      [   
+        Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemIcon][Icon.view "person" []]
+              , text "Bryan Cranston"
+            ]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemIcon][Icon.view "person" []]
+              , text "Aaron Paul"
+            ]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemIcon][Icon.view "person" []]
+              , text "Bob Odenkirk"
+            ]
+          ]
+      ]
+
+
+avatarsAndActions : Model -> Html Msg
+avatarsAndActions model  =
+    Lists.ul [Options.css "width" "300px" ] 
+      [   
+        Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bryan Cranston"] 
+            ]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Options.styled Html.i [][Icon.view "star" []]]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Aaron Paul"] 
+            ]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Options.styled Html.i [][Icon.view "star" []]]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bob Odenkirk"] 
+            ]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Options.styled Html.i [][Icon.view "star" []]]
+          ]
+      ]
+
+avatarsAndControls : Model -> Html Msg
+avatarsAndControls model  =
+    Lists.ul [Options.css "width" "300px" ] 
+      [   
+        Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , text "Bryan Cranston"
+            ]
+            ,
+            Options.span [Lists.itemSecondaryAction]
+            [
+              Toggles.checkbox Mdl [0] model.mdl
+                [ Toggles.value True
+                ]
+                [ ]              
+            ]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , text "Aaron Paul"
+            ]
+            ,
+            Options.span [Lists.itemSecondaryAction]
+            [
+              Toggles.radio Mdl [0] model.mdl
+                [ Toggles.value False, Options.css "display" "inline"
+                ]
+                [ ]              
+            ]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , text "Bob Odenkirk"
+            ]
+            ,
+            Options.span [Lists.itemSecondaryAction]
+            [
+              Toggles.switch Mdl [0] model.mdl
+                [ Toggles.value True
+                ]
+                [ ]              
+            ]
+          ]
+      ]
+
+twoLines : Model -> Html Msg
+twoLines model  =
+    Lists.ul [Options.css "width" "300px" ] 
+      [   
+        Lists.li [Lists.twoLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+                Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bryan Cranston"]
+              , Options.span [ Lists.itemSubTitle][text "62 Episodes"]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      , Lists.li [Lists.twoLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Aaron Paul"] 
+              , Options.span [ Lists.itemSubTitle][text "62 Episodes"]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      , Lists.li [Lists.twoLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bob Odenkirk"] 
+              , Options.span [ Lists.itemSubTitle][text "62 Episodes"]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      ]
+
+
+threeLines : Model -> Html Msg
+threeLines model  =
+    Lists.ul [Options.css "width" "650px" ] 
+      [   
+        Lists.li [Lists.threeLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+                Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bryan Cranston"]
+              , Options.span [ Lists.itemTextBody][text "Bryan Cranston played the role of Walter in Breaking Bad. He is also known
+        for playing Hal in Malcom in the Middle."]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      , Lists.li [Lists.threeLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Aaron Paul"] 
+              , Options.span [ Lists.itemTextBody][text "Aaron Paul played the role of Jesse in Breaking Bad. He also featured in
+        the Need For Speed Movie."]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      , Lists.li [Lists.threeLine]
+          [
+            Options.span [Lists.itemPrimaryContent]
+            [
+              Options.styled Html.i [Lists.itemAvatar][Icon.view "person" [Icon.size36]]
+              , Options.span [][text "Bob Odenkirk"] 
+              , Options.span [ Lists.itemTextBody][text "Bob Odinkrik played the role of Saul in Breaking Bad. Due to public fondness for the
+            character, Bob stars in his own show now, called Better Call Saul."]
+            ]
+          , Options.span [Lists.itemSecondaryContent]
+            [
+              Options.span [ Lists.itemSecondaryInfo][text "Actor"]
+            , Options.styled Html.a [Lists.itemSecondaryAction][Icon.view "star" []]
+            ]
+          ]
+      ]
+
 
 
 view : Model -> Html Msg
 view model  =
   [ div 
       [] 
-      [ Lists.ul [] []
+      [ 
+        h4 [][text "Simple list"]
+      , simpleList model
+      , h5 [][text "Example code for simple list:"]
+      , code
+      , h4 [][text "Icon list"]
+      , icons model
+      , h4 [][text "Avatars and Actions"]
+      , avatarsAndActions model
+      , h4 [][text "Avatars and Controls"]
+      , avatarsAndControls model
+      ,  h4 [][text "Two lines"]
+      , twoLines model  
+      , h4 [][text "Three lines"]
+      , threeLines model  
       ]
   ]
   |> Page.body2 "Lists" srcUrl intro references
@@ -60,8 +310,9 @@ view model  =
 
 intro : Html m
 intro = 
-  Page.fromMDL "https://www.getmdl.io/components/index.html#Lists-section" """
-> ...
+  Page.fromMDL "https://www.getmdl.io/components/index.html#lists-section" """
+> Lists present multiple line items vertically as a single continuous element.
+> Refer the Material Design Spec to know more about the content options.
 """ 
 
 
@@ -73,8 +324,28 @@ srcUrl =
 references : List (String, String)
 references = 
   [ Page.package "http://package.elm-lang.org/packages/debois/elm-mdl/latest/Material-Lists"
-  , Page.mds "https://www.google.com/design/spec/components/Lists.html"
-  , Page.mdl "https://www.getmdl.io/components/index.html#Lists"
+  , Page.mds "https://material.google.com/components/lists.html"
+  , Page.mdl "https://www.getmdl.io/components/index.html#lists"
   ]
 
 
+code : Html msg
+code =
+  Code.code """
+    Lists.ul [Options.css "width" "300px"] 
+      [   
+        Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Bryan Cranston"]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Aaron Paul"]
+          ]
+      , Lists.li []
+          [
+            Options.span [Lists.itemPrimaryContent][text "Bob Odenkirk"]
+          ]
+      ]
+
+  """
--- src/Material/Lists.elm
+++ src/Material/Lists.elm
@@ -1,19 +1,40 @@
-module Material.Lists exposing (ul)
+module Material.Lists exposing (
+    ul, li
+  , twoLine, threeLine
+  , itemPrimaryContent, itemAvatar, itemIcon, itemSecondaryContent, itemSecondaryInfo, itemSecondaryAction, itemTextBody, itemSubTitle
+  )
 
--- TEMPLATE. Copy this to a file for your component, then update.
 
-{-| From the [Material Design Lite documentation](http://www.getmdl.io/components/#TEMPLATE-section):
+{-| From the [Material Design Lite documentation](http://www.getmdl.io/components/index.html#lists-section):
 
-> ...
+> Lists present multiple line items vertically as a single continuous element.
+> Refer the Material Design Spec to know more about the content options.
 
 See also the
-[Material Design Specification]([https://www.google.com/design/spec/components/TEMPLATE.html).
+[Material Design Specification]([https://material.google.com/components/lists.html).
 
-Refer to [this site](http://debois.github.io/elm-mdl#/template)
+Refer to [this site](http://debois.github.io/elm-mdl#/lists)
 for a live demo.
 
-@docs ul
+Example use: 
 
+    ul []
+        [ li [  ] [  ]
+        , li [  ] [  ]
+        , li [  ] [  ]
+        ]
+
+# Main list elements
+@docs ul, li
+
+# List item configuration for multiple lines
+@docs twoLine, threeLine
+
+# Content configuration for one or multiple lines
+@docs itemPrimaryContent, itemAvatar, itemIcon
+
+# Content configuration for multiple lines
+@docs itemSecondaryContent, itemSecondaryInfo, itemSecondaryAction, itemTextBody, itemSubTitle
 -}
 
 
@@ -22,28 +43,123 @@ import Html.Events as Html
 import Html exposing (Html, Attribute)
 
 import Parts exposing (Indexed)
-import Material.Options as Options exposing (Property, Style, cs)
+import Material.Options as Options exposing (Property, Style, cs, nop)
 
-{-| Main ul constructor. Example use: 
 
-    ul []
-        [ li [  ] [  ]
-        , li [  ] [  ]
-        , li [  ] [  ]
-        ]
+{-| Main list function
 -}
-
 ul
   :  List (Property {} m)
   -> List (Html m)
   -> Html m
 ul options nodes =
   let
-    summary =
+   summary =
       Options.collect {} options
   in
     Options.apply summary Html.ul
-    [ cs "mdl-list" ]
+    [ cs "mdl-list"
+    ]
     [
     ]
     nodes
+
+
+type ListItemLinesVariant 
+  = OneLine
+  | TwoLine
+  | ThreeLine
+
+type alias ListItemConfig =
+  { listItemLinesVariant : ListItemLinesVariant
+  }
+
+defaultListItemConfig : ListItemConfig
+defaultListItemConfig =
+  { listItemLinesVariant = OneLine
+  }
+
+
+{-| List item should have two lines
+-}
+twoLine : Property { a | listItemLinesVariant : ListItemLinesVariant } m
+twoLine =
+  listItemLines TwoLine
+
+{-| List item should have three lines
+-}
+threeLine : Property { a | listItemLinesVariant : ListItemLinesVariant } m
+threeLine =
+  listItemLines ThreeLine
+
+{-| List item should have the given number of lines
+-}
+listItemLines : ListItemLinesVariant -> Property { a | listItemLinesVariant : ListItemLinesVariant } m
+listItemLines lines =
+  Options.set <| \self -> { self | listItemLinesVariant = lines }
+
+
+{-| List item
+-}
+li
+  :  List (Property ListItemConfig m)
+  -> List (Html m)
+  -> Html m
+li options nodes =
+  let
+    ({ config } as summary) =
+      Options.collect defaultListItemConfig options
+  in
+    Options.apply summary Html.li
+    [ case config.listItemLinesVariant of
+          OneLine -> Options.nop
+          TwoLine -> cs "mdl-list__item--two-line"
+          ThreeLine -> cs "mdl-list__item--three-line"
+    , cs "mdl-list__item" 
+    ]
+    [
+    ]
+    nodes    
+
+
+{-| Defines the primary content sub-division
+-}
+itemPrimaryContent : Property a m
+itemPrimaryContent = cs "mdl-list__item-primary-content"
+
+{-| Defines the avatar sub-division
+-}
+itemAvatar : Property a m
+itemAvatar = cs "mdl-list__item-avatar"
+
+{-| Defines the icon sub-division
+-}
+itemIcon : Property a m
+itemIcon = cs "mdl-list__item-icon"
+
+{-| Defines the secondary content sub-division.	Requires an item with twoLine or threeLine.
+-}
+itemSecondaryContent : Property a m
+itemSecondaryContent = cs "mdl-list__item-secondary-content"
+
+{-| Defines the information sub-division.	Requires an item with twoLine or threeLine.
+-}
+itemSecondaryInfo : Property a m
+itemSecondaryInfo = cs "mdl-list__item-secondary-info"
+
+{-| Defines the Action sub-division.	Requires an item with twoLine or threeLine.
+-}
+itemSecondaryAction : Property a m
+itemSecondaryAction = cs "mdl-list__item-secondary-action"
+
+{-| Defines the Text Body sub-division.	Requires an item with twoLine or threeLine.
+-}
+itemTextBody : Property a m
+itemTextBody = cs "mdl-list__item-text-body"
+
+{-| Defines the Sub title sub-division.	Requires an item with twoLine or threeLine. 
+-}
+itemSubTitle : Property a m
+itemSubTitle = cs "mdl-list__item-sub-title"
+
+
