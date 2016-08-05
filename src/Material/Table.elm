b408419afe71a83ced8274c10cfc4bb264987dfd
OvermindDL1 <overminddl1@gmail.com>
2016-08-05 14:44:05 -0600

    Table demo url fix
    This one was extra broken, missing a `)` too.

--- src/Material/Table.elm
+++ src/Material/Table.elm
@@ -30,7 +30,7 @@ See also the
 [Material Design Specification]([https://www.google.com/design/spec/components/data-tables.html).
 
 Refer to
-[this this](https://debois.github.io/elm-mdl/#/tables
+[this this](https://debois.github.io/elm-mdl/#tables)
 for a live demo.
 
 # HTML
