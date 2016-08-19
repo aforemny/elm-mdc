08d8f5e6eddeb66145681c419bdba479bbfc861b
Ville Penttinen <villem.penttinen@gmail.com>
2016-08-19 12:51:48 +0300

    Refactor Chip API
    New Chip API uses opaque type to ensure that only specific
    types are allowed when creating a chip.
    
    Add ability to add event handlers to Chip and also the
    delete action has been added as a property on the Chip itself
    rather than provided as Content. This is because there should really
    only be a single delete action and it does not really need other functionality
    other than responding to either as a link or to clicks.

--- demo/Demo/Chips.elm
+++ demo/Demo/Chips.elm
@@ -3,6 +3,7 @@ module Demo.Chips exposing (..)
 import Platform.Cmd exposing (Cmd, none)
 import Html exposing (..)
 import Html.Attributes as Html
+import Html.Events as Html
 
 import Material.Chip as Chip
 import Material.Icon as Icon
@@ -10,22 +11,32 @@ import Material.Grid as Grid
 import Material.Color as Color
 import Material.Options as Options exposing (css, cs)
 import Material
+import Material.Textfield as Textfield
+import Material.Helpers as Helpers
+import Material.Card as Card
 
 import Demo.Page as Page
 import Demo.Code as Code
 
+import Dict exposing (Dict)
+
+import Json.Decode as Json
 
 -- MODEL
 
 
 type alias Model =
   { mdl : Material.Model
+  , chips : Dict Int String
+  , value : String
   }
 
 
 model : Model
 model =
   { mdl = Material.model
+  , chips = Dict.fromList [(0, "Chip 0")]
+  , value = ""
   }
 
 
@@ -34,13 +45,73 @@ model =
 
 type Msg
   = Mdl (Material.Msg Msg)
+  | AddChip Int String
+  | RemoveChip Int
+  | NoOp
+  | KeyUp Int
+  | Input String
+  | ChipClick Int
 
+
+
+lastIndex : Dict Int b -> Int
+lastIndex dict =
+  Dict.keys dict
+    |> List.reverse
+    |> List.head
+    |> Maybe.withDefault 0
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
+    NoOp ->
+        (model, Cmd.none)
+
+    Input value ->
+      ( { model | value = value }, Cmd.none )
+
+    KeyUp keyCode ->
+      let
+        -- _ = Debug.log "KeyUp" keyCode
+
+        cmds =
+          if keyCode == 13 then
+            [ Helpers.cmd (AddChip ((lastIndex model.chips) + 1) model.value)
+            , Helpers.cmd (Input "")
+            ]
+          else
+            []
+      in
+        (model, Cmd.batch cmds)
+
     Mdl action' ->
       Material.update action' model
+
+    ChipClick index ->
+      let
+        _ = Debug.log "CLICKED" index
+      in
+        (model, Cmd.none)
+
+    AddChip index content ->
+      let
+        -- _ = Debug.log "Adding chip" index
+        -- _ = Debug.log "Content" content
+
+        model' =
+          { model | chips = Dict.insert index content model.chips }
+      in
+        (model', Cmd.none)
+
+    RemoveChip index ->
+      let
+        _ = Debug.log "Removing chip" index
+
+        model' =
+          { model | chips = Dict.remove index model.chips }
+
+      in
+        (model', Cmd.none)
 
 
 -- VIEW
@@ -60,100 +131,106 @@ demoContainer (html, code) =
 
 demoChips : List (Grid.Cell m)
 demoChips =
-  [ ( Chip.chip []
-        [ Chip.text []
+  [ ( Chip.chipSpan []
+        [ Chip.content []
             [ text "Basic Chip" ]
         ]
     , """
-      Chip.chip []
-        [ Chip.text []
+      Chip.chipSpan []
+        [ Chip.content []
             [ text "Basic Chip" ]
         ]
        """
     )
-  , ( Chip.chip
-        [ Chip.deletable ]
-        [ Chip.text []
+  , ( Chip.chipSpan
+        [ Chip.deleteIcon "cancel" ]
+        [ Chip.content []
             [ text "Deletable Chip" ]
-        , Chip.actionButton []
-            [ Icon.view "cancel" [] ]
         ]
     , """
-      Chip.chip
-        [ Chip.deletable ]
-        [ Chip.text []
+      Chip.chipSpan
+        [ Chip.deleteIcon "cancel" ]
+        [ Chip.content []
             [ text "Deletable Chip" ]
-        , Chip.actionButton []
-            [ Icon.view "cancel" [] ]
         ]
        """
     )
-  , ( Chip.chip' Html.button
-        [ Options.type' "button" ]
-        [ Chip.text []
+  , ( Chip.chipButton []
+        [ Chip.content []
             [ text "Button Chip" ]
         ]
     , """
-      Chip.chip' Html.button
-        [ Options.type' "button" ]
-        [ Chip.text []
+      Chip.chipButton []
+        [ Chip.content []
             [ text "Button Chip" ]
         ]
        """
     )
-  ,  ( Chip.chip
-          [ Chip.contact ]
-          [ Chip.contactItem
-              [ Color.background Color.primary
-              , Color.text Color.white 
-              ]
-              [ text "A" ]
-          , Chip.text []
-              [ text "Contact Chip" ]
-          ]
-     , """
-        Chip.chip
-          [ Chip.contact ]
-          [ Chip.contactItem
+  ,  ( Chip.chipSpan []
+          [ Chip.contact Html.span
               [ Color.background Color.primary
               , Color.text Color.white
               ]
               [ text "A" ]
-          , Chip.text []
+          , Chip.content []
+              [ text "Contact Chip" ]
+          ]
+     , """
+        Chip.chipSpan []
+          [ Chip.contact Html.span
+              [ Color.background Color.primary
+              , Color.text Color.white
+              ]
+              [ text "A" ]
+          , Chip.content []
               [ text "Contact Chip" ]
           ]
         """
      )
-  , ( Chip.chip
-        [ Chip.contact
-        , Chip.deletable
-        ]
-        [ Chip.contactItem
+  , ( Chip.chipSpan
+        [ Chip.deleteLink "#chips"]
+        [ Chip.contact Html.span
             [ Color.background Color.primary
             , Color.text Color.white
             ]
             [ text "A" ]
         , Chip.text []
-            [ text "Deletable Contact Chip" ]
-        , Chip.action Html.a
-            [ Options.attribute <| Html.href "#chips" ]
-            [ Icon.view "cancel" [] ]
+            "Deletable Contact Chip"
         ]
     , """
-      Chip.chip
-        [ Chip.contact
-        , Chip.deletable
+      Chip.chipSpan
+        [ Chip.deleteLink "#chips"
         ]
-        [ Chip.contactItem
+        [ Chip.contact Html.span
             [ Color.background Color.primary
             , Color.text Color.white
             ]
             [ text "A" ]
-        , Chip.text []
+        , Chip.content []
             [ text "Deletable Contact Chip" ]
-        , Chip.action Html.a
-            [ Options.attribute <| Html.href "#chips" ]
-            [ Icon.view "cancel" [] ]
+        ]
+       """
+    )
+  , ( Chip.chipSpan []
+        [ Chip.contact Html.img
+            [ Options.css "background-image" "url('assets/images/elm.png')"
+            , Options.css "background-size" "cover"
+            ]
+            [ text "" ]
+        , Chip.content []
+            [ text "Chip with image" ]
+        ]
+    , """
+      Chip.chipSpan []
+        [ Chip.contact Html.img
+            [ Options.css "background-image"
+                "url('assets/images/elm.png')"
+            , Options.css "background-size"
+                "cover"
+            ]
+            [ text "" ]
+        , Chip.content []
+            [ text "Chip with image" ]
         ]
        """
     )
@@ -162,21 +239,58 @@ demoChips =
 
 view : Model -> Html Msg
 view model  =
-  [ Html.div []
-      [ Html.p [] [text "Example use:"]
-      , Grid.grid []
-          ( Grid.cell
-              [ Grid.size Grid.All 12]
-              [ Code.code [ css "margin" "24px 0" ]
-                  """
-                   import Material.Chip as Chip
-                   """
-              ]
-          :: demoChips)
+  let
+    examples =
+      [ Html.div []
+          [ Html.p [] [text "Example use:"]
+          , Grid.grid [] <|
+              ( Grid.cell
+                  [ Grid.size Grid.All 12]
+                  [ Code.code [ css "margin" "24px 0" ]
+                      """
+                      import Material.Chip as Chip
+                      """
+                  ]
+              :: demoChips)
+          ]
+
       ]
 
-  ]
-  |> Page.body2 "Chips" srcUrl intro references
+    interactive =
+      [ Options.styled Html.div
+          [ Color.background Color.white
+          ]
+          ((Dict.toList model.chips
+            |> List.map (\ (index, value) ->
+                           Chip.chipSpan
+                             [ Options.css "margin" "17.5px 0"
+                             , Chip.onClick (ChipClick index)
+                             , Chip.deleteClick (RemoveChip index)
+                             ]
+                             [ Chip.contact Html.img
+                                 [ Options.css "background-image" "url('assets/images/elm.png')"
+                                 , Options.css "background-size" "cover"
+                                 ]
+                                 [ text "" ]
+                             , Chip.content []
+                                 [ text value ]
+                             ]
+                        )
+          ) ++
+             [ Textfield.render Mdl [0] model.mdl
+                 [ Textfield.label "Search"
+                 , Options.css "margin-left" "20px"
+                 , Textfield.on "keyup" (Json.map KeyUp Html.keyCode)
+                 , Textfield.onInput Input
+                 , Textfield.value model.value
+                 ]
+             ]
+          )
+      ]
+  in
+    Page.body1' "Chips" srcUrl intro references
+      examples
+      interactive
 
 
 intro : Html m
--- src/Material/Chip.elm
+++ src/Material/Chip.elm
@@ -1,10 +1,9 @@
 module Material.Chip exposing
-  ( chip, chip'
-  , text, text'
-  , contactItem, contactItem'
-  , action, actionButton
-  , contact, deletable
-  , HtmlElement
+  ( Property, HtmlElement, Item
+  , chip, chipButton, chipSpan
+  , content , text , contact
+  , deleteIcon , deleteLink , deleteClick
+  , on, onClick
   )
 
 {-| From the [Material Design Lite documentation](http://www.getmdl.io/components/index.html#chips-section):
@@ -19,105 +18,317 @@ Refer to [this site](http://debois.github.io/elm-mdl/#chips)
 for a live demo.
 
 
+# Types
+@docs Property, HtmlElement, Item
+
 # Elements
-@docs chip, chip'
-@docs text, text'
-@docs contactItem, contactItem'
-@docs action
-@docs actionButton
+@docs chip, chipButton, chipSpan
+@docs content , text , contact
 
-# Attributes
-@docs contact, deletable
-
-# Helper
-@docs HtmlElement
+# Properties
+@docs deleteIcon , deleteLink , deleteClick
+@docs on, onClick
 
 -}
 
+-- ( chip, chip'
+-- , text, text'
+-- , contactItem, contactItem'
+-- , action, actionButton
+-- , contact, deletable
+-- , HtmlElement
+-- )
+
 import Html exposing (Attribute, Html)
+import Html.Events
+import Html.Attributes
 import Material.Options as Options exposing (cs)
+import Material.Icon as Icon
+import Material.Options.Internal as Internal
+import Json.Decode as Json
 
 
-{-| Helper for a `Html m` function. e.g. `Html.div`
+{-| Alias for a `Html m` function. e.g. `Html.div`
 -}
 type alias HtmlElement msg =
   List (Attribute msg) -> List (Html msg) -> Html msg
 
 
+type alias Config msg =
+  { deleteIcon : Maybe String
+  , deleteLink : Maybe (Html.Attribute msg)
+  , deleteClick : Maybe (Html.Attribute msg)
+  , listeners : List (Html.Attribute msg)
+  }
 
-{-| Create a chip contained in the given element
+
+defaultConfig : Config msg
+defaultConfig =
+  { deleteIcon = Nothing
+  , deleteLink = Nothing
+  , deleteClick = Nothing
+  , listeners = []
+  }
+
+
+{-| Properties for Chip options.
 -}
-chip' : HtmlElement msg -> List (Options.Property c msg) -> List (Html msg) -> Html msg
-chip' element props content =
-  Options.styled element
-    (cs "mdl-chip" :: props)
-    content
+type alias Property msg =
+  Options.Property (Config msg) msg
+
+
+{-| Chip can contain only specific kind of content
+-}
+type Item msg
+  = Contact (HtmlElement msg) (List (Property msg)) (List (Html msg))
+  | Text (List (Property msg)) (List (Html msg))
+  | Action (HtmlElement msg) (List (Property msg)) (List (Html msg))
+
+
+{-| Set the icon for the delete action
+-}
+deleteIcon : String -> Property msg
+deleteIcon icon =
+  Options.set (\config -> { config | deleteIcon = Just icon })
+
+
+{-| Set the link for the delete action.
+
+NOTE. This turns the action to `Html.a` element
+-}
+deleteLink : String -> Property msg
+deleteLink link =
+  Options.set (\config -> { config | deleteLink = Just (Html.Attributes.href link) })
+
+
+{-| Set the `onClick` for the delete action
+
+NOTE. This stops propagation and prevents default to stop `Chip.onClick` from being called
+when this is clicked
+-}
+deleteClick : msg -> Property msg
+deleteClick msg =
+  -- We want to prevent the onClick of the Chip itself being called when clicking on the delete action
+  Options.set
+    (\config ->
+      { config
+        | deleteClick =
+            Just
+              (Html.Events.onWithOptions "click"
+                { stopPropagation = True, preventDefault = True }
+                (Json.succeed msg)
+              )
+      }
+    )
+
+
+{-| Add custom event handlers
+-}
+on : String -> Json.Decoder m -> Property m
+on event decoder =
+  Options.set
+    (\config ->
+      { config
+        | listeners = config.listeners ++ [ (Html.Events.on event decoder) ]
+      }
+    )
+
+
+{-| Add an `onClick` handler to the chip
+-}
+onClick : msg -> Property msg
+onClick msg =
+  on "click" (Json.succeed msg)
+
+
+type alias Priority =
+  Int
+
+
+priority : Item a -> Priority
+priority item =
+  case item of
+    Contact _ _ _ ->
+      0
+
+    Text _ _ ->
+      1
+
+    Action _ _ _ ->
+      2
+
+
+{-| Renders a given `Item`
+-}
+renderItem : Item msg -> Html msg
+renderItem item =
+  case item of
+    Contact element props content ->
+      Options.styled element
+        (cs "mdl-chip__contact" :: props)
+        content
+
+    Text props content ->
+      Options.styled Html.span
+        (cs "mdl-chip__text" :: props)
+        content
+
+    Action element props content ->
+      Options.styled element
+        (cs "mdl-chip__action" :: props)
+        content
+
+
+hasValue : Maybe a -> Bool
+hasValue m =
+  case m of
+    Just _ ->
+      True
+
+    Nothing ->
+      False
+
+
+getActionElement : Config msg -> Maybe (Item msg)
+getActionElement config =
+  let
+    hasIcon =
+      hasValue config.deleteIcon
+
+    hasLink =
+      hasValue config.deleteLink
+
+    hasClick =
+      hasValue config.deleteClick
+
+    icon =
+      if hasIcon then
+        Maybe.withDefault "" config.deleteIcon
+      else if (hasLink || hasClick) then
+        Maybe.withDefault "cancel" config.deleteIcon
+      else
+        ""
+
+    actionElement =
+      if hasLink then
+        Html.a
+      else
+        Html.span
+
+    link =
+      case config.deleteLink of
+        Just l ->
+          Internal.attribute l
+
+        Nothing ->
+          Options.nop
+
+    click =
+      case config.deleteClick of
+        Just c ->
+          Internal.attribute c
+
+        Nothing ->
+          Options.nop
+
+    isDeletable =
+      hasIcon || hasLink || hasClick
+  in
+    if isDeletable then
+      action actionElement
+        [ link, click ]
+        [ Icon.view icon [] ]
+        |> Just
+    else
+      Nothing
+
+
+{-| Creates a chip using `Html.button`
+-}
+chipButton : List (Property msg) -> List (Item msg) -> Html msg
+chipButton props =
+  chip Html.button (Options.type' "button" :: props)
 
 
 {-| Creates a chip using `Html.span`
 -}
-chip : List (Options.Property c msg) -> List (Html msg) -> Html msg
-chip =
-  chip' Html.span
+chipSpan : List (Property msg) -> List (Item msg) -> Html msg
+chipSpan =
+  chip Html.span
 
 
-{-| Create a chip text contained in the given element
+{-| Create a chip contained in the given element
 -}
-text' : HtmlElement msg -> List (Options.Property c msg) -> List (Html msg) -> Html msg
-text' element props content =
-  Options.styled element
-    (cs "mdl-chip__text" :: props)
-    content
+chip : HtmlElement msg -> List (Property msg) -> List (Item msg) -> Html msg
+chip element props items =
+  let
+    summary =
+      Options.collect defaultConfig props
+
+    config =
+      summary.config
+
+    listeners =
+      config.listeners
+
+    action =
+      getActionElement config
+
+    isDeletable =
+      hasValue action
+
+    withIcon =
+      (case action of
+        Just a ->
+          [ a ]
+
+        Nothing ->
+          []
+      )
+        ++ items
+
+    content =
+      withIcon
+        |> List.sortBy priority
+        |> List.map renderItem
+
+    isContact =
+      List.any (\x -> priority x == 0) items
+  in
+    Options.styled' element
+      ([ cs "mdl-chip"
+       , cs "mdl-chip--contact" `Options.when` isContact
+       , cs "mdl-chip--deletable" `Options.when` isDeletable
+       ]
+        ++ props
+      )
+      listeners
+      content
 
 
-{-| Create a text element using `Html.span`
+{-| Generate chip content
 -}
-text : List (Options.Property c msg) -> List (Html msg) -> Html msg
-text =
-  text' Html.span
+content : List (Property msg) -> List (Html msg) -> Item msg
+content =
+  Text
+
+
+{-| Shorthand for `Chip.content [] [ Html.text "text" ]`
+-}
+text : List (Property msg) -> String -> Item msg
+text props txt =
+  Text props [ Html.text txt ]
 
 
 {-| Create a chip action contained in the given element
 -}
-action : HtmlElement msg -> List (Options.Property c msg) -> List (Html msg) -> Html msg
-action element props content =
-  Options.styled element
-    (cs "mdl-chip__action" :: props)
-    content
-
-
-{-| Create a chip action `Html.button`
--}
-actionButton : List (Options.Property c msg) -> List (Html msg) -> Html msg
-actionButton props =
-  action Html.button ((Options.type' "button") :: props)
+action : HtmlElement msg -> List (Property msg) -> List (Html msg) -> Item msg
+action =
+  Action
 
 
 {-| Create a chip contact contained in the given element
 -}
-contactItem' : HtmlElement msg -> List (Options.Property c msg) -> List (Html msg) -> Html msg
-contactItem' element props content =
-  Options.styled element
-    (cs "mdl-chip__contact" :: props)
-    content
-
-
-{-| Create a chip contact using `Html.span`
--}
-contactItem : List (Options.Property c msg) -> List (Html msg) -> Html msg
-contactItem =
-  contactItem' Html.span
-
-
-{-| Defines a chip as a contact style chip
--}
-contact : Options.Property c m
+contact : HtmlElement msg -> List (Property msg) -> List (Html msg) -> Item msg
 contact =
-  cs "mdl-chip--contact"
-
-
-{-| Defines a chip as a deletable style chip
--}
-deletable : Options.Property c m
-deletable =
-  cs "mdl-chip--deletable"
+  Contact
