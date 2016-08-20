70fd065e3c2988e9b27345ef94f8e22a8e92fdb5
Ville Penttinen <villem.penttinen@gmail.com>
2016-08-20 09:54:28 +0300

    Update group implementation to one provided by @debois

--- src/Dispatch.elm
+++ src/Dispatch.elm
@@ -94,18 +94,6 @@ upsert key value func dict =
   else
     Dict.insert key value dict
 
-{-|
--}
-group : List ( comparable, a ) -> List ( comparable, List a )
-group items =
-  items
-    |> List.foldr
-       (\( k, v ) accum ->
-         upsert k [ v ] (Maybe.map (\a -> v :: a)) accum
-       )
-       Dict.empty
-    |> Dict.toList
-
 
 {-| Combines decoders for events and returns event listeners
 -}
@@ -117,3 +105,49 @@ listeners lift items =
   items
     |> List.map (\( event, decoders ) -> onEvt lift event decoders)
     |> List.filterMap identity
+
+
+{-| Group a list of pairs based on the first item
+-}
+group : List ( a, b ) -> List ( a, List b )
+group = group' []
+
+
+split
+    : a
+    -> List b
+    -> List ( a, b )
+    -> List ( a, b )
+    -> ( List b, List ( a, b ) )
+split k0 same differ xs =
+  case xs of
+    [] ->
+      (same, differ)
+
+    ((k, v) as x) :: xs ->
+      if k == k0 then
+        split k0 (v :: same) differ xs
+      else
+        split k0 same (x :: differ) xs
+
+
+group' : List ( a, List b ) -> List ( a, b ) -> List ( a, List b )
+group' acc items =
+  case items of
+    [] ->
+      acc
+
+    [(k,v)] ->
+      (k, [v]) :: acc
+
+    [(k1,v1), (k2,v2)] ->
+      if k1 == k2 then
+        (k1, [v2, v1]) :: acc
+      else
+        (k2, [v2]) :: (k1, [v1]) :: acc
+
+    (k,v) :: xs ->
+      let (same, different) =
+            split k [v] [] xs
+      in
+        group' ((k, same) :: acc) different
