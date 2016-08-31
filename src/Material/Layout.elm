810155e644451535b2338bcff9e620f3b35a4026
Søren Debois <soren.debois@gmail.com>
2016-08-31 23:15:58 +0200

    Re-reverted Layout API changes (on master)

--- src/Material/Layout.elm
+++ src/Material/Layout.elm
@@ -467,14 +467,14 @@ spacer = div [class "mdl-layout-spacer"] []
 
 {-| Title in header row or drawer.
 -}
-title : List (Options.Property c m) -> List (Html m) -> Html m
+title : List (Options.Style m) -> List (Html m) -> Html m
 title styles = 
   Options.span (cs "mdl-layout__title" :: styles) 
 
 
 {-| Container for links.
 -}
-navigation : List (Options.Property c m) -> List (Html m) -> Html m
+navigation : List (Options.Style m) -> List (Html m) -> Html m
 navigation styles contents =
   nav [class "mdl-navigation"] contents
 
@@ -513,7 +513,7 @@ link styles contents =
 
 {-| Header row. 
 -}
-row : List (Options.Property c m) -> List (Html m) -> Html m
+row : List (Options.Style m) -> List (Html m) -> Html m
 row styles = 
   Options.div (cs "mdl-layout__header-row" :: styles) 
 
