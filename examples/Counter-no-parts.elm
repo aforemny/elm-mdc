ab1e8ddc6873ad5661c40529f219e130b9ddcf69
Søren Debois <soren.debois@gmail.com>
2016-12-28 08:25:12 +0100

    Upgrade examples to 0.18

--- examples/Counter-no-parts.elm
+++ examples/Counter-no-parts.elm
@@ -1,11 +1,13 @@
 {- This file shows how to use elm-mdl components as vanilla TEA components. You
-are unlikely to want this; consider looking at Counter.elm instead. 
+   are unlikely to want this; consider looking at Counter.elm instead.
 -}
+
+
+module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
 import Platform.Cmd exposing (Cmd)
-
 import Material.Button as Button
 import Material.Scheme
 import Material.Options as Options exposing (css)
@@ -14,72 +16,73 @@ import Material.Options as Options exposing (css)
 -- MODEL
 
 
-type alias Model = 
-  { count : Int
-  , increaseButtonModel : Button.Model
-  , resetButtonModel : Button.Model
-  }
+type alias Model =
+    { count : Int
+    , increaseButtonModel : Button.Model
+    , resetButtonModel : Button.Model
+    }
 
 
-model : Model 
-model = 
-  { count = 0
-  , increaseButtonModel = Button.defaultModel 
-  , resetButtonModel = Button.defaultModel 
-  }
+model : Model
+model =
+    { count = 0
+    , increaseButtonModel = Button.defaultModel
+    , resetButtonModel = Button.defaultModel
+    }
+
 
 
 -- ACTION, UPDATE
 
 
 type Msg
-  = IncreaseButtonMsg Button.Msg
-  | ResetButtonMsg Button.Msg
-  | Increase
-  | Reset
+    = IncreaseButtonMsg Button.Msg
+    | ResetButtonMsg Button.Msg
+    | Increase
+    | Reset
 
 
 increase : Model -> Model
-increase model = 
-  { model | count = model.count + 1 }
+increase model =
+    { model | count = model.count + 1 }
 
 
-reset : Model -> Model 
-reset model = 
-   { model | count = 0 }
+reset : Model -> Model
+reset model =
+    { model | count = 0 }
 
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increase -> 
-       ( { model | count = model.count + 1 } 
-       , Cmd.none
-       )
- 
-    Reset -> 
-       ( { model | count = 0 }
-       , Cmd.none
-       )
- 
-    IncreaseButtonMsg msg_ -> 
-      let 
-        (submodel, fx) =
-          Button.update msg_ model.increaseButtonModel
-      in 
-        ( { model | increaseButtonModel = submodel }
-        , Cmd.map IncreaseButtonMsg fx
-        )
+    case msg of
+        Increase ->
+            ( { model | count = model.count + 1 }
+            , Cmd.none
+            )
 
-    ResetButtonMsg msg_ -> 
-      let 
-        (submodel, fx) =
-          Button.update msg_ model.resetButtonModel 
-      in 
-        ( { model | resetButtonModel = submodel }
-        , Cmd.map ResetButtonMsg fx
-        )
+        Reset ->
+            ( { model | count = 0 }
+            , Cmd.none
+            )
+
+        IncreaseButtonMsg msg_ ->
+            let
+                ( submodel, fx ) =
+                    Button.update msg_ model.increaseButtonModel
+            in
+                ( { model | increaseButtonModel = submodel }
+                , Cmd.map IncreaseButtonMsg fx
+                )
+
+        ResetButtonMsg msg_ ->
+            let
+                ( submodel, fx ) =
+                    Button.update msg_ model.resetButtonModel
+            in
+                ( { model | resetButtonModel = submodel }
+                , Cmd.map ResetButtonMsg fx
+                )
+
 
 
 -- VIEW
@@ -87,26 +90,28 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div
-    [ style [ ("padding", "2rem") ] ]
-    [ text ("Current count: " ++ toString model.count )
-    , Button.view IncreaseButtonMsg model.increaseButtonModel
-        [ Options.onClick Increase 
-        , css "margin" "0 24px"
+    div
+        [ style [ ( "padding", "2rem" ) ] ]
+        [ text ("Current count: " ++ toString model.count)
+        , Button.view IncreaseButtonMsg
+            model.increaseButtonModel
+            [ Options.onClick Increase
+            , css "margin" "0 24px"
+            ]
+            [ text "Increase" ]
+        , Button.view ResetButtonMsg
+            model.resetButtonModel
+            [ Options.onClick Reset ]
+            [ text "Reset" ]
         ]
-        [ text "Increase" ]
-    , Button.view ResetButtonMsg model.resetButtonModel 
-        [ Options.onClick Reset ] 
-        [ text "Reset" ]
-    ]
-  |> Material.Scheme.top 
+        |> Material.Scheme.top
 
 
 main : Program Never Model Msg
 main =
-  Html.program 
-    { init = ( model, Cmd.none ) 
-    , view = view
-    , subscriptions = always Sub.none 
-    , update = update
-    }
+    Html.program
+        { init = ( model, Cmd.none )
+        , view = view
+        , subscriptions = always Sub.none
+        , update = update
+        }
