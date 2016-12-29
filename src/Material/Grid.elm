8fa699079c8ce731cd0a38a0f608da9dafdff304
Søren Debois <soren.debois@gmail.com>
2016-12-29 13:27:34 +0100

    Allow `offset 0` in Grid
    Closes #241.

--- src/Material/Grid.elm
+++ src/Material/Grid.elm
@@ -163,16 +163,16 @@ offset device k =
         c =
             case device of
                 All ->
-                    clip 1 11 k
+                    clip 0 11 k
 
                 Desktop ->
-                    clip 1 11 k
+                    clip 0 11 k
 
                 Tablet ->
-                    clip 1 7 k
+                    clip 0 7 k
 
                 Phone ->
-                    clip 1 3 k
+                    clip 0 3 k
     in
         "mdl-cell--" ++ toString c ++ "-offset" ++ suffix device |> cs
 
