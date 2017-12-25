48b9b44b1083d98472db9523dae2ab89dddbea4c
Matt Newman <mdjnewman@gmail.com>
2017-12-25 13:52:57 -0600

    Documentation fixes for Elevation, LayoutGrid and Options

--- src/Material/Elevation.elm
+++ src/Material/Elevation.elm
@@ -48,126 +48,176 @@ movement. An object’s elevation determines the appearance of its shadow.
 import Material.Options exposing (..)
 
 
+{-| Sets the elevation to 0 dp.
+-}
 z0 : Property a m
 z0 =
     z 0
 
 
+{-| Sets the elevation to 1 dp.
+-}
 z1 : Property a m
 z1 =
     z 1
 
 
+{-| Sets the elevation to 2 dp.
+-}
 z2 : Property a m
 z2 =
     z 2
 
 
+{-| Sets the elevation to 3 dp.
+-}
 z3 : Property a m
 z3 =
     z 3
 
 
+{-| Sets the elevation to 4 dp.
+-}
 z4 : Property a m
 z4 =
     z 4
 
 
+{-| Sets the elevation to 5 dp.
+-}
 z5 : Property a m
 z5 =
     z 5
 
 
+{-| Sets the elevation to 6 dp.
+-}
 z6 : Property a m
 z6 =
     z 6
 
 
+{-| Sets the elevation to 7 dp.
+-}
 z7 : Property a m
 z7 =
     z 7
 
 
+{-| Sets the elevation to 8 dp.
+-}
 z8 : Property a m
 z8 =
     z 8
 
 
+{-| Sets the elevation to 9 dp.
+-}
 z9 : Property a m
 z9 =
     z 9
 
 
+{-| Sets the elevation to 10 dp.
+-}
 z10 : Property a m
 z10 =
     z 10
 
 
+{-| Sets the elevation to 11 dp.
+-}
 z11 : Property a m
 z11 =
     z 11
 
 
+{-| Sets the elevation to 12 dp.
+-}
 z12 : Property a m
 z12 =
     z 12
 
 
+{-| Sets the elevation to 13 dp.
+-}
 z13 : Property a m
 z13 =
     z 13
 
 
+{-| Sets the elevation to 14 dp.
+-}
 z14 : Property a m
 z14 =
     z 14
 
 
+{-| Sets the elevation to 15 dp.
+-}
 z15 : Property a m
 z15 =
     z 15
 
 
+{-| Sets the elevation to 16 dp.
+-}
 z16 : Property a m
 z16 =
     z 16
 
 
+{-| Sets the elevation to 17 dp.
+-}
 z17 : Property a m
 z17 =
     z 17
 
 
+{-| Sets the elevation to 18 dp.
+-}
 z18 : Property a m
 z18 =
     z 18
 
 
+{-| Sets the elevation to 19 dp.
+-}
 z19 : Property a m
 z19 =
     z 19
 
 
+{-| Sets the elevation to 20 dp.
+-}
 z20 : Property a m
 z20 =
     z 20
 
 
+{-| Sets the elevation to 21 dp.
+-}
 z21 : Property a m
 z21 =
     z 21
 
 
+{-| Sets the elevation to 22 dp.
+-}
 z22 : Property a m
 z22 =
     z 22
 
 
+{-| Sets the elevation to 23 dp.
+-}
 z23 : Property a m
 z23 =
     z 23
 
 
+{-| Sets the elevation to 24 dp.
+-}
 z24 : Property a m
 z24 =
     z 24
@@ -178,6 +228,8 @@ z v =
     cs <| "mdc-elevation--z" ++ toString v
 
 
+{-| Applies the correct css rules to transition an element between elevations.
+-}
 transition : Property a m
 transition =
     cs "mdc-elevation--transition"
--- src/Material/LayoutGrid.elm
+++ src/Material/LayoutGrid.elm
@@ -24,6 +24,14 @@ module Material.LayoutGrid exposing
   , span2Phone
   , span3Phone
   , span4Phone
+  , span5Phone
+  , span6Phone
+  , span7Phone
+  , span8Phone
+  , span9Phone
+  , span10Phone
+  , span11Phone
+  , span12Phone
 
   , span1Tablet
   , span2Tablet
@@ -33,6 +41,10 @@ module Material.LayoutGrid exposing
   , span6Tablet
   , span7Tablet
   , span8Tablet
+  , span9Tablet
+  , span10Tablet
+  , span11Tablet
+  , span12Tablet
 
   , span1Desktop
   , span2Desktop
@@ -63,8 +75,8 @@ module Material.LayoutGrid exposing
 
 ## Cell spans
 @docs span1, span2, span3, span4, span5, span6, span7, span8, span9, span10, span11, span12
-@docs span1Phone, span2Phone, span3Phone, span4Phone
-@docs span1Tablet, span2Tablet, span3Tablet, span4Tablet, span5Tablet, span6Tablet, span7Tablet, span8Tablet
+@docs span1Phone, span2Phone, span3Phone, span4Phone, span5Phone, span6Phone, span7Phone, span8Phone, span9Phone, span10Phone, span11Phone, span12Phone
+@docs span1Tablet, span2Tablet, span3Tablet, span4Tablet, span5Tablet, span6Tablet, span7Tablet, span8Tablet, span9Tablet, span10Tablet, span11Tablet, span12Tablet
 @docs span1Desktop, span2Desktop, span3Desktop, span4Desktop, span5Desktop, span6Desktop, span7Desktop, span8Desktop, span9Desktop, span10Desktop, span11Desktop, span12Desktop
 
 ## Properties
@@ -85,275 +97,389 @@ defaultConfig =
     {}
 
 
+{-| A LayoutGrid's property.
+-}
 type alias Property m =
     Options.Property Config m
 
 
+{-| Component view.
+-}
 view : List (Property m) -> List (Html m) -> Html m
 view options =
     styled Html.div (cs "mdc-layout-grid" :: options)
 
+{-| The grid is by default center aligned. This modifier changes this behaviour.
 
+
+Note, this modifiers will have no effect when the grid already fills its container.
+-}
 alignLeft : Property m
 alignLeft =
     cs "mdc-layout-grid--align-left"
 
+{-| The grid is by default center aligned. This modifier changes this behaviour.
 
+Note, this modifiers will have no effect when the grid already fills its container.
+-}
 alignRight : Property m
 alignRight =
     cs "mdc-layout-grid--align-right"
 
 
+{-| Specifies the grid should have fixed column width.
+-}
 fixedColumnWidth : Property m
 fixedColumnWidth =
     cs "mdc-layout-grid--fixed-column-width"
 
 
+{-| When your contents need extra structure that cannot be supported by single layout grid,
+you can nest layout grid within each other. Use `inner` to nest layout grids, by wrapping around nested cell.
+-}
 inner : List (Property m) -> List (Html m) -> Html m
 inner options =
     styled Html.div (cs "mdc-layout-grid__inner" :: options)
 
 
+{-| Defines a layout grid cell.
+-}
 cell : List (Property m) -> List (Html m) -> Html m
 cell options =
     styled Html.div (cs "mdc-layout-grid__cell" :: options)
 
 
+{-| Span 1 columns.
+-}
 span1 : Property m
 span1 =
     span Nothing 1
 
 
+{-| Span 2 columns.
+-}
 span2 : Property m
 span2 =
     span Nothing 2
 
 
+{-| Span 3 columns.
+-}
 span3 : Property m
 span3 =
     span Nothing 3
 
 
+{-| Span 4 columns.
+-}
 span4 : Property m
 span4 =
     span Nothing 4
 
 
+{-| Span 5 columns.
+-}
 span5 : Property m
 span5 =
     span Nothing 5
 
 
+{-| Span 6 columns.
+-}
 span6 : Property m
 span6 =
     span Nothing 6
 
 
+{-| Span 7 columns.
+-}
 span7 : Property m
 span7 =
     span Nothing 7
 
 
+{-| Span 8 columns.
+-}
 span8 : Property m
 span8 =
     span Nothing 8
 
 
+{-| Span 9 columns.
+-}
 span9 : Property m
 span9 =
     span Nothing 9
 
 
+{-| Span 10 columns.
+-}
 span10 : Property m
 span10 =
     span Nothing 10
 
 
+{-| Span 11 columns.
+-}
 span11 : Property m
 span11 =
     span Nothing 11
 
 
+{-| Span 12 columns.
+-}
 span12 : Property m
 span12 =
     span Nothing 12
 
 
+{-| Span 1 columns on a phone.
+-}
 span1Phone : Property m
 span1Phone =
     span (Just "phone") 1
 
 
+{-| Span 2 columns on a phone.
+-}
 span2Phone : Property m
 span2Phone =
     span (Just "phone") 2
 
 
+{-| Span 3 columns on a phone.
+-}
 span3Phone : Property m
 span3Phone =
     span (Just "phone") 3
 
 
+{-| Span 4 columns on a phone.
+-}
 span4Phone : Property m
 span4Phone =
     span (Just "phone") 4
 
 
+{-| Span 5 columns on a phone.
+-}
 span5Phone : Property m
 span5Phone =
     span (Just "phone") 5
 
 
+{-| Span 6 columns on a phone.
+-}
 span6Phone : Property m
 span6Phone =
     span (Just "phone") 6
 
 
+{-| Span 7 columns on a phone.
+-}
 span7Phone : Property m
 span7Phone =
     span (Just "phone") 7
 
 
+{-| Span 8 columns on a phone.
+-}
 span8Phone : Property m
 span8Phone =
     span (Just "phone") 8
 
 
+{-| Span 9 columns on a phone.
+-}
 span9Phone : Property m
 span9Phone =
     span (Just "phone") 9
 
 
+{-| Span 10 columns on a phone.
+-}
 span10Phone : Property m
 span10Phone =
     span (Just "phone") 10
 
 
+{-| Span 11 columns on a phone.
+-}
 span11Phone : Property m
 span11Phone =
     span (Just "phone") 11
 
 
+{-| Span 12 columns on a phone.
+-}
 span12Phone : Property m
 span12Phone =
     span (Just "phone") 12
 
 
+{-| Span 1 columns on a tablet.
+-}
 span1Tablet : Property m
 span1Tablet =
     span (Just "tablet") 1
 
 
+{-| Span 2 columns on a tablet.
+-}
 span2Tablet : Property m
 span2Tablet =
     span (Just "tablet") 2
 
 
+{-| Span 3 columns on a tablet.
+-}
 span3Tablet : Property m
 span3Tablet =
     span (Just "tablet") 3
 
 
+{-| Span 4 columns on a tablet.
+-}
 span4Tablet : Property m
 span4Tablet =
     span (Just "tablet") 4
 
 
+{-| Span 5 columns on a tablet.
+-}
 span5Tablet : Property m
 span5Tablet =
     span (Just "tablet") 5
 
 
+{-| Span 6 columns on a tablet.
+-}
 span6Tablet : Property m
 span6Tablet =
     span (Just "tablet") 6
 
 
+{-| Span 7 columns on a tablet.
+-}
 span7Tablet : Property m
 span7Tablet =
     span (Just "tablet") 7
 
 
+{-| Span 8 columns on a tablet.
+-}
 span8Tablet : Property m
 span8Tablet =
     span (Just "tablet") 8
 
 
+{-| Span 9 columns on a tablet.
+-}
 span9Tablet : Property m
 span9Tablet =
     span (Just "tablet") 9
 
 
+{-| Span 10 columns on a tablet.
+-}
 span10Tablet : Property m
 span10Tablet =
     span (Just "tablet") 10
 
 
+{-| Span 11 columns on a tablet.
+-}
 span11Tablet : Property m
 span11Tablet =
     span (Just "tablet") 11
 
 
+{-| Span 12 columns on a tablet.
+-}
 span12Tablet : Property m
 span12Tablet =
     span (Just "tablet") 12
 
 
+{-| Span 1 columns on a desktop.
+-}
 span1Desktop : Property m
 span1Desktop =
     span (Just "desktop") 1
 
 
+{-| Span 2 columns on a desktop.
+-}
 span2Desktop : Property m
 span2Desktop =
     span (Just "desktop") 2
 
 
+{-| Span 3 columns on a desktop.
+-}
 span3Desktop : Property m
 span3Desktop =
     span (Just "desktop") 3
 
 
+{-| Span 4 columns on a desktop.
+-}
 span4Desktop : Property m
 span4Desktop =
     span (Just "desktop") 4
 
 
+{-| Span 5 columns on a desktop.
+-}
 span5Desktop : Property m
 span5Desktop =
     span (Just "desktop") 5
 
 
+{-| Span 6 columns on a desktop.
+-}
 span6Desktop : Property m
 span6Desktop =
     span (Just "desktop") 6
 
 
+{-| Span 7 columns on a desktop.
+-}
 span7Desktop : Property m
 span7Desktop =
     span (Just "desktop") 7
 
 
+{-| Span 8 columns on a desktop.
+-}
 span8Desktop : Property m
 span8Desktop =
     span (Just "desktop") 8
 
 
+{-| Span 9 columns on a desktop.
+-}
 span9Desktop : Property m
 span9Desktop =
     span (Just "desktop") 9
 
 
+{-| Span 10 columns on a desktop.
+-}
 span10Desktop : Property m
 span10Desktop =
     span (Just "desktop") 10
 
 
+{-| Span 11 columns on a desktop.
+-}
 span11Desktop : Property m
 span11Desktop =
     span (Just "desktop") 11
 
 
+{-| Span 12 columns on a desktop.
+-}
 span12Desktop : Property m
 span12Desktop =
     span (Just "desktop") 12
