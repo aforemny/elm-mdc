74b22fa2fdcb690e424342d6213dcbc67a0eb85f
Alexander Foremny <aforemny@posteo.de>
2018-03-05 11:15:18 +0100

    Document Textfield.HelperText

--- src/Material/Textfield/HelperText.elm
+++ src/Material/Textfield/HelperText.elm
@@ -1,11 +1,51 @@
 module Material.Textfield.HelperText exposing
     ( helperText
-    , Property
     , persistent
+    , Property
     , validationMsg
-    , Config
-    , defaultConfig
     )
+
+{-|
+Helper text gives context about a field’s input, such as how the input will be
+used. It should be visible either persistently or only on focus.
+
+
+# Resources
+
+- [Material Design guidelines: Text Fields Layout](https://material.io/guidelines/components/text-fields.html#text-fields-layout)
+- [Demo](https://aforemny.github.io/elm-mc/#text-field)
+
+
+# Example
+
+```elm
+import Material.Textfield as Textfield
+import Material.Textfield.HelperText as Textfield
+
+
+Html.div []
+    [ Textfield.view Mdc [0] model.mdc
+          [ Textfield.label "Email Adress"
+          ]
+          []
+    , Textfield.helperText
+      [ Textfield.persistent
+      , Textfield.validationMsg
+      ]
+      [ text "Help Text (possibly validation message)"
+      ]
+    ]
+```
+
+
+# Usage
+
+@docs Property
+@docs helperText
+@docs persistent
+@docs validationMsg
+
+-}
 
 
 import Html exposing (Html)
@@ -13,19 +53,12 @@ import Material.Internal.Options as Internal
 import Material.Options as Options exposing (cs, css, nop, Style, when)
 
 
--- OPTIONS
-
-
-{-| TODO
--}
 type alias Config =
     { persistent : Bool
     , validationMsg : Bool
     }
 
 
-{-| TODO
--}
 defaultConfig : Config
 defaultConfig =
     { persistent = False
@@ -33,30 +66,27 @@ defaultConfig =
     }
 
 
-{-| TODO
+{-| HelperText property.
 -}
 type alias Property m =
     Options.Property Config m
 
 
-{-| TODO
+{-| Make the helper text permanently visible.
 -}
 persistent : Property m
 persistent =
     Internal.option (\config -> { config | persistent = True })
 
 
-{-| TODO
+{-| Indicates the helper text is a validation message.
 -}
 validationMsg : Property m
 validationMsg =
     Internal.option (\config -> { config | validationMsg = True })
 
 
--- VIEW
-
-
-{-| TODO
+{-| HelperText view.
 -}
 helperText : List (Property m) -> List (Html m) -> Html m
 helperText options =
