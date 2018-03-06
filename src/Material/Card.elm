48b006b0a5444323d127f80ccea0d6ff8793d4b1
Alexander Foremny <aforemny@posteo.de>
2018-03-06 11:17:44 +0100

    Document Card

--- src/Material/Card.elm
+++ src/Material/Card.elm
@@ -17,28 +17,110 @@ module Material.Card
         , view
         )
 
-{-| Card is a component that implements the Material Design card component.
+{-|
+Card is a component that implements the Material Design card component.
 
-## Design & API Documentation
+
+# Resources
 
 - [Material Design guidelines: Cards](https://material.io/guidelines/components/cards.html)
 - [Demo](https://aforemny.github.io/elm-mdc/#cards)
 
+
+# Example
+
+```elm
+import Html exposing (text)
+import Material.Button as Button
+import Material.Card as Card
+import Material.IconToggle as IconToggle
+import Material.Options exposing (styled, css)
+import Material.Typography as Typography
+
+
+Card.view
+    [ css "width" "350px"
+    ]
+    [ Card.media
+          [ Card.aspect16To9
+          , Card.backgroundImage "images/16-9.jpg"
+          ]
+          []
+    , styled Html.div
+          [ css "padding" "1rem"
+          ]
+          [ styled Html.h2
+            [ Typography.title
+            , css "margin" "0"
+            ]
+            [ text "Our Changing Planet"
+            ]
+          , styled Html.h3
+            [ Typography.subheading1
+            , css "margin" "0"
+            ]
+            [ text "by Kurt Wagner"
+            ]
+          ]
+    , styled Html.div
+          [ css "padding" "0 1rem 8px 1rem"
+          , css "color" "rgba(0, 0, 0, 0.54)"
+          , Typography.body1
+          ]
+          [ text """
+Visit ten places on our planet that are undergoing the
+biggest changes today."""
+          ]
+    , Card.actions []
+          [ Card.actionButtons []
+                [ Button.view Mdc [0] model.mdc
+                      [ Card.actionButton
+                      , Button.ripple
+                      ]
+                      [ text "Read"
+                      ]
+                ]
+          , Card.actionIcons []
+                [ IconToggle.view Mdc [1] model.mdc
+                      [ Card.actionIcon
+                      , IconToggle.icon
+                        { on = "favorite"
+                        , off = "favorite_border"
+                        }
+                      , IconToggle.label
+                        { on = "Remove from favorites"
+                        , off = "Add to favorites"
+                        }
+                      ]
+                      []
+                ]
+          ]
+    ]
+```
+
+
+# Usage
+
+
+## Card
+
 @docs Property
 @docs view
 @docs stroked
-@docs primary
-@docs secondary
 @docs primaryAction
 
-### Card media
+
+## Card media
+
 @docs media
 @docs backgroundImage
 @docs square
-@docs aspect16-9
+@docs aspect16To9
 @docs mediaContent
 
-### Card actions
+
+## Card actions
+
 @docs actions
 @docs fullBleed
 @docs actionButtons
@@ -62,20 +144,20 @@ defaultConfig =
     {}
 
 
-{-| A card's property.
+{-| Card property.
 -}
 type alias Property m =
     Options.Property Config m
 
 
-{-| Component view.
+{-| Card view.
 -}
 view : List (Property m) -> List (Html m) -> Html m
 view options =
     Options.div (cs "mdc-card" :: options)
 
 
-{-| Removes the card's shadow and displays a hairline stroke instead
+{-| Removes the card's shadow and displays a hairline stroke instead.
 -}
 stroked : Property m
 stroked =
@@ -83,15 +165,18 @@ stroked =
 
 
 {-| The main tappable area of the card. Typically contains all card content,
-except `cardActions`. Only applicable to cards that have a primary action that
-the main surface should trigger.
+except `cardActions`.
+
+Only applicable to cards that have a primary action that the main surface
+should trigger.
 -}
 primaryAction : List (Property m) -> List (Html m) -> Html m
 primaryAction options =
     div (cs "mdc-card__primary-action"::options)
 
 
-{-| Media area that displays a custom `background-image` with `background-size: cover`
+{-| Media area that displays a custom `background-image` with `background-size:
+cover`.
 -}
 media : List (Property m) -> List (Html m) -> Html m
 media options =
@@ -101,12 +186,14 @@ media options =
     )
 
 
+{-| Sets the background image url of the card's media area.
+-}
 backgroundImage : String -> Property m
 backgroundImage url =
     css "background-image" ("url(" ++ url ++ ")")
 
 
-{-| Automatically scales the media area's height to equal its width
+{-| Automatically scales the media area's height to equal its width.
 -}
 square : Property m
 square =
@@ -114,7 +201,7 @@ square =
 
 
 {-| Automatically scales the media area's height according to its width,
-maintaining a 16:9 aspect ratio
+maintaining a 16:9 aspect ratio.
 -}
 aspect16To9 : Property m
 aspect16To9 =
@@ -122,7 +209,7 @@ aspect16To9 =
 
 
 {-| An absolutely-positioned box the same size as the media area, for
-displaying a title or icon on top of the `background-image`
+displaying a title or icon on top of the `background-image`.
 -}
 mediaContent : List (Property m) -> List (Html m) -> Html m
 mediaContent options =
@@ -132,7 +219,7 @@ mediaContent options =
     )
 
 
-{-| Row containing action buttons and/or icons
+{-| Row containing action buttons and/or icons.
 -}
 actions : List (Property m) -> List (Html m) -> Html m
 actions options =
@@ -143,7 +230,7 @@ actions options =
 
 
 {-| Removes the action area's padding and causes its only child (`actions`
-element) to consume 100% of the action area's width
+element) to consume 100% of the action area's width.
 -}
 fullBleed : Property m
 fullBleed =
@@ -151,7 +238,7 @@ fullBleed =
 
 
 {-| A group of action buttons, displayed on the left side of the card (in LTR),
-adjacent to `actionIcons`
+adjacent to `actionIcons`.
 -}
 actionButtons : List (Property m) -> List (Html m) -> Html m
 actionButtons options =
@@ -162,7 +249,7 @@ actionButtons options =
 
 
 {-| A group of supplemental icons, displayed on the right side of the card (in
-LTR), adjacent to `actionButtons`
+LTR), adjacent to `actionButtons`.
 -}
 actionIcons : List (Property m) -> List (Html m) -> Html m
 actionIcons options =
@@ -172,7 +259,7 @@ actionIcons options =
     )
 
 
-{-| An action button with text
+{-| An action button with text.
 -}
 actionButton : Button.Property m
 actionButton =
@@ -180,7 +267,7 @@ actionButton =
 
 
 
-{-| An action icon with text
+{-| An action icon with text.
 -}
 actionIcon : IconToggle.Property m
 actionIcon =
