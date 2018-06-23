82cae81d614d510c218bacd26904392fdea81f61
Alexander Foremny <aforemny@posteo.de>
2018-06-23 12:23:18 +0200

    Update StartPage

--- demo/Demo/Startpage.elm
+++ demo/Demo/Startpage.elm
@@ -18,7 +18,6 @@ import Demo.Startpage.Svg.Menu as MenuSvg
 import Demo.Startpage.Svg.Radio as RadioSvg
 import Demo.Startpage.Svg.Ripple as RippleSvg
 import Demo.Startpage.Svg.Select as SelectSvg
-import Demo.Startpage.Svg.Shape as ShapeSvg
 import Demo.Startpage.Svg.Slider as SliderSvg
 import Demo.Startpage.Svg.Snackbar as SnackbarSvg
 import Demo.Startpage.Svg.Switch as SwitchSvg
@@ -29,7 +28,6 @@ import Demo.Startpage.Svg.TopAppBar as TopAppBarSvg
 import Demo.Startpage.Svg.Typography as TypographySvg
 import Demo.Url exposing (Url(..))
 import Html exposing (Html, text)
-import Html.Attributes as Html
 import Material.ImageList as ImageList
 import Material.Options as Options exposing (cs, css, id, styled, when)
 
@@ -38,161 +36,161 @@ view : Page m -> Html m
 view page =
     styled Html.div
         []
-        [ page.toolbar "MATERIAL COMPONENTS FOR THE WEB"
+        [ page.toolbar "Material Components for the Web"
         , styled Html.nav
             []
             [ ImageList.view
                 [ id "catalog-image-list" ]
-                ([ { url = Button
-                   , icon = ButtonSvg.view
-                   , title = "Button"
-                   , subtitle = "Raised and flat buttons"
-                   }
-                 , { url = Card
-                   , icon = CardSvg.view
-                   , title = "Card"
-                   , subtitle = "Various card layout styles"
-                   }
-                 , { url = Checkbox
-                   , icon = CheckboxSvg.view
-                   , title = "Checkbox"
-                   , subtitle = "Multi-selection controls"
-                   }
-                 , { url = Chips
-                   , icon = ChipsSvg.view
-                   , title = "Chips"
-                   , subtitle = "Chips"
-                   }
-                 , { url = Dialog
-                   , icon = DialogSvg.view
-                   , title = "Dialog"
-                   , subtitle = "Secondary text"
-                   }
-                 , { url = Drawer
-                   , icon = DrawerSvg.view
-                   , title = "Drawer"
-                   , subtitle = "Various drawer styles"
-                   }
-                 , { url = Elevation
-                   , icon = ElevationSvg.view
-                   , title = "Elevation"
-                   , subtitle = "Shadow for different elevations"
-                   }
-                 , { url = Fabs
-                   , icon = FabSvg.view
-                   , title = "FAB"
-                   , subtitle = "The primary action in an application"
-                   }
-                 , { url = IconToggle
-                   , icon = IconButtonSvg.view
-                   , title = "Icon Button"
-                   , subtitle = "Toggling icon states"
-                   }
-                 , { url = ImageList
-                   , icon = ImageListSvg.view
-                   , title = "Image List"
-                   , subtitle = "An Image List consists of several items, each containing an image and optionally supporting content (i.e. a text label)"
-                   }
-                 , { url = LayoutGrid
-                   , icon = LayoutGridSvg.view
-                   , title = "Layout Grid"
-                   , subtitle = "Grid and gutter support"
-                   }
-                 , { url = List
-                   , icon = ListSvg.view
-                   , title = "List"
-                   , subtitle = "Item layouts in lists"
-                   }
-                 , { url = LinearProgress
-                   , icon = LinearProgressSvg.view
-                   , title = "Linear progress"
-                   , subtitle = "Fills from 0% to 100%, represented by bars"
-                   }
-                 , { url = Menu
-                   , icon = MenuSvg.view
-                   , title = "Menu"
-                   , subtitle = "Pop over menus"
-                   }
-                 , { url = RadioButton
-                   , icon = RadioSvg.view
-                   , title = "Radio"
-                   , subtitle = "Single selection controls"
-                   }
-                 , { url = Ripple
-                   , icon = RippleSvg.view
-                   , title = "Ripple"
-                   , subtitle = "Touch ripple"
-                   }
-                 , { url = Select
-                   , icon = SelectSvg.view
-                   , title = "Select"
-                   , subtitle = "Popover selection menus"
-                   }
-                 , { url = Slider
-                   , icon = SliderSvg.view
-                   , title = "Slider"
-                   , subtitle = "Range Controls"
-                   }
-                 , { url = Snackbar
-                   , icon = SnackbarSvg.view
-                   , title = "Snackbar"
-                   , subtitle = "Transient messages"
-                   }
-                 , { url = Switch
-                   , icon = SwitchSvg.view
-                   , title = "Switch"
-                   , subtitle = "On off switches"
-                   }
-                 , { url = Tabs
-                   , icon = TabsSvg.view
-                   , title = "Tabs"
-                   , subtitle = "Tabs with support for icon and text labels"
-                   }
-                 , { url = TextField
-                   , icon = TextFieldSvg.view
-                   , title = "Text field"
-                   , subtitle = "Single and multiline text fields"
-                   }
-                 , { url = Theme
-                   , icon = ThemeSvg.view
-                   , title = "Theme"
-                   , subtitle = "Using primary and accent colors"
-                   }
-                 , { url = TopAppBar Nothing
-                   , icon = TopAppBarSvg.view
-                   , title = "Top App Bar"
-                   , subtitle = "Container for items such as application title, navigation icon, and action items."
-                   }
-                 , { url = Typography
-                   , icon = TypographySvg.view
-                   , title = "Typography"
-                   , subtitle = "Type hierarchy"
-                   }
-                 ]
-                    |> List.map
-                        (\{ url, title, subtitle, icon } ->
-                            ImageList.item
-                                [ Options.onClick (page.navigate url)
-                                , css "cursor" "pointer"
-                                , css "width" "calc(100% / 4 - 8.25px)"
-                                , css "margin" "4px"
-                                ]
-                                [ styled Html.div
-                                    [ cs "catalog-image-link" ]
-                                    [ ImageList.imageAspectContainer
-                                        [ css "padding-bottom" "100%"
-                                        ]
-                                        [ ImageList.divImage [] [ icon ] ]
-                                    , ImageList.supporting []
-                                        [ ImageList.label
-                                            [ cs
-                                                "catalog-image-list-label"
-                                            ]
-                                            [ text title ]
-                                        ]
-                                    ]
-                                ]
-                        )
+                ( List.map
+                      (\{ url, title, subtitle, icon } ->
+                          ImageList.item
+                              [ Options.onClick (page.navigate url)
+                              , css "cursor" "pointer"
+                              , css "width" "calc(100% / 4 - 8.25px)"
+                              , css "margin" "4px"
+                              ]
+                              [ styled Html.div
+                                  [ cs "catalog-image-link" ]
+                                  [ ImageList.imageAspectContainer
+                                      [ css "padding-bottom" "100%"
+                                      ]
+                                      [ ImageList.divImage [] [ icon ] ]
+                                  , ImageList.supporting []
+                                      [ ImageList.label
+                                          [ cs
+                                              "catalog-image-list-label"
+                                          ]
+                                          [ text title ]
+                                      ]
+                                  ]
+                              ]
+                      )
+                      [ { url = Button
+                       , icon = ButtonSvg.view
+                       , title = "Button"
+                       , subtitle = "Raised and flat buttons"
+                       }
+                     , { url = Card
+                       , icon = CardSvg.view
+                       , title = "Card"
+                       , subtitle = "Various card layout styles"
+                       }
+                     , { url = Checkbox
+                       , icon = CheckboxSvg.view
+                       , title = "Checkbox"
+                       , subtitle = "Multi-selection controls"
+                       }
+                     , { url = Chips
+                       , icon = ChipsSvg.view
+                       , title = "Chips"
+                       , subtitle = "Chips"
+                       }
+                     , { url = Dialog
+                       , icon = DialogSvg.view
+                       , title = "Dialog"
+                       , subtitle = "Secondary text"
+                       }
+                     , { url = Drawer
+                       , icon = DrawerSvg.view
+                       , title = "Drawer"
+                       , subtitle = "Various drawer styles"
+                       }
+                     , { url = Elevation
+                       , icon = ElevationSvg.view
+                       , title = "Elevation"
+                       , subtitle = "Shadow for different elevations"
+                       }
+                     , { url = Fabs
+                       , icon = FabSvg.view
+                       , title = "FAB"
+                       , subtitle = "The primary action in an application"
+                       }
+                     , { url = IconToggle
+                       , icon = IconButtonSvg.view
+                       , title = "Icon Button"
+                       , subtitle = "Toggling icon states"
+                       }
+                     , { url = ImageList
+                       , icon = ImageListSvg.view
+                       , title = "Image List"
+                       , subtitle = "An Image List consists of several items, each containing an image and optionally supporting content (i.e. a text label)"
+                       }
+                     , { url = LayoutGrid
+                       , icon = LayoutGridSvg.view
+                       , title = "Layout Grid"
+                       , subtitle = "Grid and gutter support"
+                       }
+                     , { url = List
+                       , icon = ListSvg.view
+                       , title = "List"
+                       , subtitle = "Item layouts in lists"
+                       }
+                     , { url = LinearProgress
+                       , icon = LinearProgressSvg.view
+                       , title = "Linear progress"
+                       , subtitle = "Fills from 0% to 100%, represented by bars"
+                       }
+                     , { url = Menu
+                       , icon = MenuSvg.view
+                       , title = "Menu"
+                       , subtitle = "Pop over menus"
+                       }
+                     , { url = RadioButton
+                       , icon = RadioSvg.view
+                       , title = "Radio"
+                       , subtitle = "Single selection controls"
+                       }
+                     , { url = Ripple
+                       , icon = RippleSvg.view
+                       , title = "Ripple"
+                       , subtitle = "Touch ripple"
+                       }
+                     , { url = Select
+                       , icon = SelectSvg.view
+                       , title = "Select"
+                       , subtitle = "Popover selection menus"
+                       }
+                     , { url = Slider
+                       , icon = SliderSvg.view
+                       , title = "Slider"
+                       , subtitle = "Range Controls"
+                       }
+                     , { url = Snackbar
+                       , icon = SnackbarSvg.view
+                       , title = "Snackbar"
+                       , subtitle = "Transient messages"
+                       }
+                     , { url = Switch
+                       , icon = SwitchSvg.view
+                       , title = "Switch"
+                       , subtitle = "On off switches"
+                       }
+                     , { url = Tabs
+                       , icon = TabsSvg.view
+                       , title = "Tabs"
+                       , subtitle = "Tabs with support for icon and text labels"
+                       }
+                     , { url = TextField
+                       , icon = TextFieldSvg.view
+                       , title = "Text field"
+                       , subtitle = "Single and multiline text fields"
+                       }
+                     , { url = Theme
+                       , icon = ThemeSvg.view
+                       , title = "Theme"
+                       , subtitle = "Using primary and accent colors"
+                       }
+                     , { url = TopAppBar Nothing
+                       , icon = TopAppBarSvg.view
+                       , title = "Top App Bar"
+                       , subtitle = "Container for items such as application title, navigation icon, and action items."
+                       }
+                     , { url = Typography
+                       , icon = TypographySvg.view
+                       , title = "Typography"
+                       , subtitle = "Type hierarchy"
+                       }
+                     ]
                 )
             ]
         ]
