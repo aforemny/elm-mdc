22700dad85dc689dbee9252865aeade3d639d9b4
Alexander Foremny <aforemny@posteo.de>
2018-11-05 20:02:50 +0100

    Update shell.nix
    Update to nixos-18.09

--- shell.nix
+++ shell.nix
@@ -1,15 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  nixpkgs-unstable = import /home/aforemny/src/nixpkgs/default.nix {};
-in
 with pkgs;
 with stdenv;
 mkDerivation {
   name = "elm-mdc";
-  buildInputs = [
-    # Elm 0.19 is available in the nixpkgs-unstable channel for now
-    nixpkgs-unstable.elmPackages.elm
-    nixpkgs-unstable.elmPackages.elm-format
-    nodejs-9_x
-  ];
+  buildInputs = [ elmPackages.elm elmPackages.elm-format nodejs-10_x ];
 }
