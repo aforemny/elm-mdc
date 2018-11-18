5d7a18f690ad15dfad123098310befd8f4b4550a
Alexander Foremny <aforemny@posteo.de>
2018-11-18 21:01:55 +0100

    Use babel to compile elm-mdc.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
   output: {
       path: path.resolve(__dirname, './'),
     filename: 'elm-mdc.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 };
 
