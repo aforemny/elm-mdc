0ec0e338093dea8f0a1ce2f8d048f3da9b392b92
Alexander Foremny <aforemny@posteo.de>
2019-01-27 20:39:44 +0100

    Update Demo.ImageList

--- demo/Demo/ImageList.elm
+++ demo/Demo/ImageList.elm
@@ -1,5 +1,7 @@
 module Demo.ImageList exposing (Model, Msg(..), defaultModel, update, view)
 
+import Demo.Helper.Hero as Hero
+import Demo.Helper.ResourceLink as ResourceLink
 import Demo.Page as Page exposing (Page)
 import Html exposing (Html, text)
 import Material
@@ -30,137 +32,143 @@ update lift msg model =
             Material.update (lift << Mdc) msg_ model
 
 
+standardImageList : Html m
+standardImageList =
+    ImageList.view
+        [ ImageList.withTextProtection
+        , css "max-width" "900px"
+        ]
+        (List.map standardItem standardImages)
+
+
+masonryImageList : Html m
+masonryImageList =
+    ImageList.view
+        [ ImageList.masonry
+        , css "max-width" "900px"
+        , css "column-count" "5"
+        , css "column-gap" "16px"
+        ]
+        (List.map masonryItem masonryImages)
+
+
+imageListHeroItem : Html m
+imageListHeroItem =
+    ImageList.item
+        [ css "width" "calc(100% / 5 - 4.2px)"
+        , css "margin" "2px"
+        ]
+        [ ImageList.imageAspectContainer []
+            [ ImageList.divImage [ css "background-color" "black" ] []
+            ]
+        ]
+
+
+standardItem : String -> Html m
+standardItem url =
+    ImageList.item
+        [ css "width" "calc(100% / 5 - 4.2px)"
+        , css "margin" "2px"
+        ]
+        [ ImageList.imageAspectContainer
+            [ css "padding-bottom" "66.66667%"
+            ]
+            [ ImageList.image [ ImageList.src url ] []
+            , ImageList.supporting [] [ ImageList.label [] [ text "Text label" ] ]
+            ]
+        ]
+
+
+masonryItem : String -> Html m
+masonryItem url =
+    ImageList.item []
+        [ ImageList.image [ ImageList.src url ] []
+        , ImageList.label [] [ text "Text label" ]
+        ]
+
+
 view : (Msg m -> m) -> Page m -> Model m -> Html m
 view lift page model =
-    let
-        imageListHeroItem =
-            ImageList.item
-                [ css "width" "calc(100% / 5 - 4.2px)"
-                , css "margin" "2px"
-                ]
-                [ ImageList.imageAspectContainer []
-                    [ ImageList.divImage
-                        [ css "background-color" "black"
-                        ]
-                        []
-                    ]
-                ]
-
-        standardImages =
-            [ "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/1.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/3.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/4.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/5.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/6.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/7.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/8.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/9.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/10.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/11.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/12.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/13.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/14.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/15.jpg"
-            ]
-
-        masonryImages =
-            [ "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/16.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/1.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/1.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/2.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/3.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/4.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/3.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/5.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/4.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/6.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/5.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/7.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/6.jpg"
-            , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/7.jpg"
-            ]
-
-        standardItem url =
-            ImageList.item
-                [ css "width" "calc(100% / 5 - 4.2px)"
-                , css "margin" "2px"
-                ]
-                [ ImageList.imageAspectContainer
-                    [ css "padding-bottom" "66.66667%"
-                    ]
-                    [ ImageList.image
-                        [ ImageList.src url
-                        ]
-                        []
-                    , ImageList.supporting []
-                        [ ImageList.label [] [ text "Text label" ]
-                        ]
-                    ]
-                ]
-
-        masonryItem url =
-            ImageList.item []
-                [ ImageList.image
-                    [ ImageList.src url
-                    ]
-                    []
-                , ImageList.label [] [ text "Text label" ]
-                ]
-
-        standardImageList index =
-            example "Standard Image List with Text Protection" <|
-                ImageList.view
-                    [ ImageList.withTextProtection
-                    , css "max-width" "900px"
-                    ]
-                    (List.map standardItem standardImages)
-
-        masonryImageList index =
-            example "Masonry Image List"
-                (ImageList.view
-                    [ ImageList.masonry
-                    , css "max-width" "900px"
-                    , css "column-count" "5"
-                    , css "column-gap" "16px"
-                    ]
-                    (List.map masonryItem masonryImages)
-                )
-
-        example title imageList =
-            styled Html.div
-                [ css "padding" "0 24px 16px"
-                ]
-                [ styled Html.div
-                    [ Typography.title
-                    , css "padding" "48px 16px 24px"
-                    ]
-                    [ text title
-                    ]
-                , imageList
-                ]
-    in
     page.body "Image List"
         "Image lists display a collection of images in an organized grid."
-        [ Page.hero []
+        [ Hero.view []
             [ ImageList.view
                 [ css "width" "300px"
                 ]
                 (List.repeat 15 imageListHeroItem)
             ]
-        , styled Html.div
-            [ cs "demo-wrapper"
+        , styled Html.h2
+            [ Typography.headline6
+            , css "border-bottom" "1px solid rgba(0,0,0,.87)"
             ]
-            [ styled Html.h1
-                [ Typography.display2
-                , css "padding-left" "36px"
-                , css "padding-top" "64px"
-                , css "padding-bottom" "8px"
-                ]
-                [ text "Demos"
-                ]
-            , standardImageList [ 1 ]
-            , masonryImageList [ 2 ]
+            [ text "Resources"
+            ]
+        , ResourceLink.view
+            { link = "https://material.io/go/design-image-list"
+            , title = "Material Design Guidelines"
+            , icon = "images/material.svg"
+            , altText = "Material Design Guidelines icon"
+            }
+        , ResourceLink.view
+            { link = "https://material.io/components/web/catalog/image-lists/"
+            , title = "Documentation"
+            , icon = "images/ic_drive_document_24px.svg"
+            , altText = "Documentation icon"
+            }
+        , ResourceLink.view
+            { link = "https://github.com/material-components/material-components-web/tree/master/packages/mdc-image-list"
+            , title = "Source Code (Material Components Web)"
+            , icon = "images/ic_code_24px.svg"
+            , altText = "Source Code"
+            }
+        , Page.demos
+            [ styled Html.h3
+                [ Typography.subtitle1 ]
+                [ text "Standard Image List with Text Protection" ]
+            , standardImageList
+            , styled Html.h3
+                [ Typography.subtitle1 ]
+                [ text "Masonry Image List" ]
+            , masonryImageList
             ]
         ]
+
+
+standardImages : List String
+standardImages =
+    [ "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/1.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/3.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/4.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/5.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/6.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/7.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/8.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/9.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/10.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/11.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/12.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/13.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/14.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/15.jpg"
+    ]
+
+
+masonryImages : List String
+masonryImages =
+    [ "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/16.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/1.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/1.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/2.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/3.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/4.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/3.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/5.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/4.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/6.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/5.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/2x3/7.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/6.jpg"
+    , "https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/7.jpg"
+    ]
