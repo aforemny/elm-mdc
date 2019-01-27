d8be273f444d4d83d807b5a54afdc6964bb62a90
Alexander Foremny <aforemny@posteo.de>
2019-01-27 22:52:31 +0100

    Update Demo.Switch

--- demo/Demo/Switch.elm
+++ demo/Demo/Switch.elm
@@ -1,5 +1,7 @@
 module Demo.Switch exposing (Model, Msg(..), defaultModel, update, view)
 
+import Demo.Helper.Hero as Hero
+import Demo.Helper.ResourceLink as ResourceLink
 import Demo.Page as Page exposing (Page)
 import Dict exposing (Dict)
 import Html exposing (Html, text)
@@ -7,6 +9,7 @@ import Material
 import Material.FormField as FormField
 import Material.Options as Options exposing (cs, css, for, styled, when)
 import Material.Switch as Switch
+import Material.Typography as Typography
 import Platform.Cmd exposing (Cmd, none)
 
 
@@ -19,7 +22,7 @@ type alias Model m =
 defaultModel : Model m
 defaultModel =
     { mdc = Material.defaultModel
-    , switches = Dict.empty
+    , switches = Dict.fromList [ ( "switch-hero-switch", True ) ]
     }
 
 
@@ -36,120 +39,95 @@ update lift msg model =
 
         Toggle index ->
             let
-                switch =
-                    Dict.get index model.switches
-                        |> Maybe.withDefault False
-                        |> not
-
                 switches =
-                    Dict.insert index switch model.switches
+                    Dict.update index
+                        (\state ->
+                            Just <|
+                                case state of
+                                    Just True ->
+                                        False
+
+                                    _ ->
+                                        True
+                        )
+                        model.switches
             in
             ( { model | switches = switches }, Cmd.none )
 
 
+isOn : Material.Index -> Model m -> Bool
+isOn index model =
+    Dict.get index model.switches
+        |> Maybe.withDefault False
+
+
+heroSwitch : (Msg m -> m) -> Model m -> Html m
+heroSwitch lift model =
+    let
+        index =
+            "switch-hero-switch"
+    in
+    FormField.view []
+        [ Switch.view (lift << Mdc)
+            index
+            model.mdc
+            [ Options.onClick (lift (Toggle index))
+            , Switch.on |> when (isOn index model)
+            ]
+            []
+        , styled Html.label [ Options.for index ] [ text "off/on" ]
+        ]
+
+
+exampleSwitch : (Msg m -> m) -> Model m -> Html m
+exampleSwitch lift model =
+    let
+        index =
+            "switch-example-switch"
+    in
+    FormField.view []
+        [ Switch.view (lift << Mdc)
+            index
+            model.mdc
+            [ Options.onClick (lift (Toggle index))
+            , Switch.on |> when (isOn index model)
+            ]
+            []
+        , styled Html.label [ Options.for index ] [ text "off/on" ]
+        ]
+
+
 view : (Msg m -> m) -> Page m -> Model m -> Html m
 view lift page model =
-    let
-        example options =
-            styled Html.div
-                (cs "example"
-                    :: css "display" "block"
-                    :: css "margin" "16px"
-                    :: css "padding" "40px"
-                    :: options
-                )
-    in
     page.body "Switch"
         "Switches communicate an action a user can take. They are typically placed throughout your UI, in places like dialogs, forms, cards, and toolbars."
-        [ Page.hero []
-            [ Html.span []
-                [ let
-                    index =
-                        "switch-hero-switch"
-
-                    on =
-                        Dict.get index model.switches
-                            |> Maybe.withDefault False
-                  in
-                  Switch.view (lift << Mdc)
-                    index
-                    model.mdc
-                    [ Options.onClick (lift (Toggle index))
-                    , Switch.on |> when on
-
-                    --, Switch.label "off/on"
-                    ]
-                    []
-                ]
-            , styled Html.label
-                [ for "switch-hero-switch"
-                , css "margin-left" "10px"
-                ]
-                [ text "off/on" ]
+        [ Hero.view [] [ heroSwitch lift model ]
+        , styled Html.h2
+            [ Typography.headline6
+            , css "border-bottom" "1px solid rgba(0,0,0,.87)"
             ]
-        , example
-            [ css "background-color" "#eee"
+            [ text "Resources"
             ]
-            [ styled Html.h2
-                [ css "margin-left" "0"
-                , css "margin-top" "0"
-                ]
-                [ text "Enabled" ]
-            , let
-                id =
-                    "switch-default-switch"
-
-                on =
-                    Dict.get id model.switches
-                        |> Maybe.withDefault False
-              in
-              FormField.view []
-                [ Switch.view (lift << Mdc)
-                    id
-                    model.mdc
-                    [ Options.onClick (lift (Toggle id))
-                    , Switch.on |> when on
-                    ]
-                    []
-                , styled Html.label
-                    [ Options.for id
-                    , css "font-size" "16px"
-                    ]
-                    [ text "off/on"
-                    ]
-                ]
-            ]
-        , example
-            [ css "background-color" "#eee"
-            ]
-            [ styled Html.h2
-                [ css "margin-left" "0"
-                , css "margin-top" "0"
-                ]
-                [ text "Disabled"
-                ]
-            , let
-                id =
-                    "switch-disabled-switch"
-
-                on =
-                    Dict.get id model.switches
-                        |> Maybe.withDefault False
-              in
-              FormField.view []
-                [ Switch.view (lift << Mdc)
-                    id
-                    model.mdc
-                    [ Options.onClick (lift (Toggle id))
-                    , Switch.disabled
-                    ]
-                    []
-                , styled Html.label
-                    [ Options.for "id"
-                    , css "font-size" "16px"
-                    ]
-                    [ text "off/on"
-                    ]
-                ]
+        , ResourceLink.view
+            { link = "https://material.io/go/design-switches"
+            , title = "Material Design Guidelines"
+            , icon = "images/material.svg"
+            , altText = "Material Design Guidelines icon"
+            }
+        , ResourceLink.view
+            { link = "https://material.io/components/web/catalog/input-controls/switches/"
+            , title = "Documentation"
+            , icon = "images/ic_drive_document_24px.svg"
+            , altText = "Documentation icon"
+            }
+        , ResourceLink.view
+            { link = "https://github.com/material-components/material-components-web/tree/master/packages/mdc-switch"
+            , title = "Source Code (Material Components Web)"
+            , icon = "images/ic_code_24px.svg"
+            , altText = "Source Code"
+            }
+        , Page.demos
+            [ styled Html.h3 [ Typography.subtitle1 ] [ text "Switch" ]
+            , exampleSwitch lift model
             ]
         ]
