dce41d3c82046c5947015cd280c16a130fa2db8d
Alexander Foremny <aforemny@posteo.de>
2019-01-27 20:15:12 +0100

    Update Demo.Elevation

--- demo/Demo/Elevation.elm
+++ demo/Demo/Elevation.elm
@@ -1,10 +1,13 @@
 module Demo.Elevation exposing (Model, Msg(..), defaultModel, update, view)
 
+import Demo.Helper.Hero as Hero
+import Demo.Helper.ResourceLink as ResourceLink
 import Demo.Page as Page exposing (Page)
 import Html exposing (Html, text)
 import Material
 import Material.Elevation as Elevation
 import Material.Options as Options exposing (cs, css, styled, when)
+import Material.Typography as Typography
 
 
 type alias Model m =
@@ -33,10 +36,6 @@ update lift msg model =
             Material.update (lift << Mdc) msg_ model
 
 
-
--- VIEW
-
-
 view : (Msg m -> m) -> Page m -> Model m -> Html m
 view lift page model =
     let
@@ -59,7 +58,7 @@ view lift page model =
     in
     page.body "Elevation"
         "Elevation is the relative depth, or distance, between two surfaces along the z-axis."
-        [ Page.hero []
+        [ Hero.view []
             [ heroSurface
                 [ Elevation.z0
                 ]
@@ -73,6 +72,30 @@ view lift page model =
                 ]
                 [ text "Raised 16dp" ]
             ]
+        , styled Html.h2
+            [ Typography.headline6
+            , css "border-bottom" "1px solid rgba(0,0,0,.87)"
+            ]
+            [ text "Resources"
+            ]
+        , ResourceLink.view
+            { link = "https://material.io/go/design-elevation"
+            , title = "Material Design Guidelines"
+            , icon = "images/material.svg"
+            , altText = "Material Design Guidelines icon"
+            }
+        , ResourceLink.view
+            { link = "https://material.io/components/web/catalog/elevation/"
+            , title = "Documentation"
+            , icon = "images/ic_drive_document_24px.svg"
+            , altText = "Documentation icon"
+            }
+        , ResourceLink.view
+            { link = "https://github.com/material-components/material-components-web/tree/master/packages/mdc-elevation"
+            , title = "Source Code (Material Components Web)"
+            , icon = "images/ic_code_24px.svg"
+            , altText = "Source Code"
+            }
         , Page.demos
             [ styled Html.div
                 [ cs "elevation-demo-container"
