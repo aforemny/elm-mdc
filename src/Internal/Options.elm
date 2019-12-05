6b879f8ac4a5f7ea76dacea58713bbbf5ce9d3e8
Berend de Boer <berend@pobox.com>
2019-12-05 16:58:43 +1300

    recollect reversed all attributes, making overrides impossible, due to how collect1 works, so revert all to make sure everything is in original order

--- src/Internal/Options.elm
+++ src/Internal/Options.elm
@@ -109,9 +109,19 @@ collect1 opt acc =
 
 
 recollect : Summary c m -> List (Property c m) -> Summary c m
-recollect =
-    List.foldl collect1
-
+recollect summary properties =
+    let
+        reversed_summary = List.foldl collect1 summary properties
+        new_summary =
+            { classes = List.reverse reversed_summary.classes
+            , css = List.reverse reversed_summary.css
+            , attrs = List.reverse reversed_summary.attrs
+            , internal = List.reverse reversed_summary.internal
+            , dispatch = reversed_summary.dispatch
+            , config = reversed_summary.config
+            }
+    in
+        new_summary
 
 collect : c -> List (Property c m) -> Summary c m
 collect =
@@ -178,7 +188,7 @@ addAttributes summary attrs =
         all =
             summary.attrs
                 ++ style
-                ++ List.map Html.Attributes.class (List.reverse summary.classes)
+                ++ List.map Html.Attributes.class summary.classes
                 ++ attrs
                 ++ summary.internal
                 ++ Dispatch.toAttributes summary.dispatch
